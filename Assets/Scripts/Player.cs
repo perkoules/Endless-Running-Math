94f4ed0553121ad1327e73ce04e2af636dcbbc95
PeriklisP <v8192218@live.tees.ac.uk>
2020-05-08 11:43:11 +0100

    Player Animator

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Playables;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] private Camera PlayerCamera;
+
+    private bool bGameStarted = false;
+    private Rigidbody PlayerRigidBody;
+    private Animator playerAnimator;
+
+    public Vector3 AddedVelocity = new Vector3(0, 0, 200);
+
+    CharacterController characterController;
+
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+
+        PlayerRigidBody = GetComponent<Rigidbody>();
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (characterController.isGrounded)
+        {
+            // We are grounded, so recalculate
+            // move direction directly from axes
+
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        // Move the controller
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+
+    void PlayerMovement(bool bStartRunning)
+    {
+        if (bStartRunning)
+        {
+            playerAnimator.SetBool("hasGameStarted", bStartRunning);
+            PlayerRigidBody.velocity = AddedVelocity * 2;
+        }
+    }
+}
