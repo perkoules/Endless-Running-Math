e3c3f28a792c33136ed2fbc21f7e61129bcb4fc6
PeriklisP <v8192218@live.tees.ac.uk>
2020-05-08 21:38:26 +0100

    UX & UI
    Basic implementation of main menu (button: Play,Instructions, quit)
    Basic UI of where the math question and options(buttons)  will appear

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    
+    public void LoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+    
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
