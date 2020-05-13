75aab65f5ff2ca2bc5a5e1aa89a92d58aa348bb8
PeriklisP <v8192218@live.tees.ac.uk>
2020-05-13 10:44:41 +0100

    Added background music
    By Slaking_97 from freesound.org

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -14,4 +14,8 @@ public class MainMenu : MonoBehaviour
         Application.Quit();
     }
 
+    public void StopAnyMusic()
+    {
+        Destroy(GameObject.FindGameObjectWithTag("Music"));
+    }
 }
---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    private AudioSource _audioSource;
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        _audioSource = GetComponent<AudioSource>();
+        PlayMusic();
+    }
+
+    public void PlayMusic()
+    {
+        if (_audioSource.isPlaying) return;
+        _audioSource.Play();
+    }
+
+    public void StopMusic()
+    {
+        _audioSource.Stop();
+    }
+}
